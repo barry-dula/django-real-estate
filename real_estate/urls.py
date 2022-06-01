1901e2aea6201402e3383d97fef9a4b29db07ee9
barry-dula <barry.dula@gmail.com>
2022-06-01 18:31:18 +0100

    configure django logging

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("supersecret/", admin.site.urls),
+   path('admin/', admin.site.urls),
+   
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
